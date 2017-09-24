31c4958d57a3ed04644b6e2eb7a3a7ca749937a8
judithgottschalk <gottschalk.judith@gmail.com>
2017-09-24 10:25:53 +0200

    elapp with database and view

---
+++ elapp/views.py
@@ -0,0 +1,15 @@
+# -*- coding: utf-8 -*-
+from __future__ import unicode_literals
+
+from django.shortcuts import render
+from django.http import HttpResponse
+from django.template import loader
+from .models import User
+
+def login(request):
+    user_list = User.objects.all()
+    template = loader.get_template('elapp/index.html')
+    context = {
+        'user_list' : user_list,
+    }
+    return HttpResponse(template.render(context, request))
